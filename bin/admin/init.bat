aefb1ee185bd35c3ae6f75f1164a7d5b44b2153f
Duc Che <ducchetrongminh@gmail.com>
2022-09-10 16:51:05 +0700

    update init

--- bin/admin/init.bat
+++ bin/admin/init.bat
@@ -9,7 +9,7 @@ pip install dbt-bigquery
 pip install sqlfluff
 
 :: Requirements files are used to hold the result from pip freeze for the purpose of achieving Repeatable Installs. In this case, your requirement file contains a pinned version of everything that was installed when pip freeze was run.
-pip freeze > requirements.txt
+pip freeze > bin/requirements.txt
 
 :: Initiate DBT
 dbt init
