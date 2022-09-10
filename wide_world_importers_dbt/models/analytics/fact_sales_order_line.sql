ef5af49560b1a52899a3de8117e795889afdf6bf
Duc Che <ducchetrongminh@gmail.com>
2022-09-10 17:29:23 +0700

    sua bai 0102

--- wide_world_importers_dbt/models/analytics/fact_sales_order_line.sql
+++ wide_world_importers_dbt/models/analytics/fact_sales_order_line.sql
@@ -1,12 +1,6 @@
-{#
-Ở bài trước, mình đã viết query để lấy 3 cột và đặt lại tên. Trong bài này, mình cần phải thêm "derived facts" (số liệu được tính toán).
-Yêu cầu:
-- Viết query để có thể tính gross_amount (doanh thu) trên model
-#}
-
-
 SELECT 
   order_line_id AS sales_order_line_id
   , quantity
   , unit_price
+  , quantity * unit_price AS gross_amount
 FROM `duckdata-320210.wide_world_importers.sales__order_lines`
