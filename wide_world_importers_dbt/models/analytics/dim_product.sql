202593b30f33de8ebd5b363d6fc002bd97423db5
Duc Che <ducchetrongminh@gmail.com>
2022-09-12 07:32:06 +0700

    fix lỗi

--- wide_world_importers_dbt/models/analytics/dim_product.sql
+++ wide_world_importers_dbt/models/analytics/dim_product.sql
@@ -9,7 +9,7 @@ Yêu cầu #0104a:
 
 
 SELECT 
-    CAST(stock_item_id AS INT) AS product_id
+    stock_item_id AS product_id
     , stock_item_name AS product_name
     , brand AS brand_name
 FROM `duckdata-320210.wide_world_importers.warehouse__stock_items`
