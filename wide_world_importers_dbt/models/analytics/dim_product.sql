ae15150532e465a5c586ec91f01b53e218c79d5f
Duc Che <ducchetrongminh@gmail.com>
2022-09-12 07:29:14 +0700

    lesson 0104a

--- wide_world_importers_dbt/models/analytics/dim_product.sql
+++ wide_world_importers_dbt/models/analytics/dim_product.sql
@@ -1,5 +1,9 @@
 {#
+Trong thực tế, data source khi được đưa về data lake có thể không phải là data type (loại dữ liệu) đúng như mình nghĩ. Một ví dụ thường gặp đó là một cột dạng số (numeric) khi đưa về data lake bị chuyển thành dạng chữ (text). Do đó, để ĐẢM BẢO đúng data type, chúng ta cần phải chuyển đổi data type, dù hiện tại có thể nó đang đúng. 
 
+Yêu cầu #0104a:
+- Tìm syntax chuyển đổi data type
+- Chuyển đổi data type của bảng này
 #}
 
 
