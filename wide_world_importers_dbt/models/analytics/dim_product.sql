d8adc6b55fbf9306bfaafb5e5b723dd0565f7d84
Duc Che <ducchetrongminh@gmail.com>
2022-09-13 07:30:58 +0700

    sửa 0105a: tách source

--- wide_world_importers_dbt/models/analytics/dim_product.sql
+++ wide_world_importers_dbt/models/analytics/dim_product.sql
@@ -7,8 +7,13 @@ Yêu cầu #0105a:
 #}
 
 
+WITH dim_product__source AS (
+  SELECT *
+  FROM `duckdata-320210.wide_world_importers.warehouse__stock_items`
+)
+
 SELECT 
   CAST(stock_item_id AS INTEGER) AS product_id
   , CAST(stock_item_name AS STRING) AS product_name
   , CAST(brand AS STRING) AS brand_name
-FROM `duckdata-320210.wide_world_importers.warehouse__stock_items`
+FROM dim_product__source
