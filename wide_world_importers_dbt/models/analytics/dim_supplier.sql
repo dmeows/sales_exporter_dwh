6b757b8f5414bb454da45cfa2359bb4588260497
Duc Che <ducchetrongminh@gmail.com>
2022-09-13 23:53:22 +0700

    yêu cầu #0107a

---
+++ wide_world_importers_dbt/models/analytics/dim_supplier.sql
@@ -0,0 +1,13 @@
+{#
+Yêu cầu #0107a:
+- Xem thông tin và dữ liệu của bảng "purchasing__suppliers".
+- Lấy dữ liệu cho bảng "dim_supplier". Tương tự như "stg_fact_sales_order", mình cũng cần xử lý trước khi JOIN vào "dim_product". Tuy nhiên, bảng "dim_supplier" có khả năng sẽ được sử dụng nên mình sẽ để ở "analytics".
+
+| Tên gốc       | Tên mới       |
+|---------------|---------------|
+| supplier_id   | supplier_id   |
+| supplier_name | supplier_name |
+
+#}
+
+
