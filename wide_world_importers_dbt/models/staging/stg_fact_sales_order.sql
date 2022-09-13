30c6e1b13b5358897228543d48e35443b8169036
Duc Che <ducchetrongminh@gmail.com>
2022-09-13 23:03:34 +0700

    sửa bài 0106a: xử lý sales order

---
+++ wide_world_importers_dbt/models/staging/stg_fact_sales_order.sql
@@ -0,0 +1,24 @@
+WITH fact_sales_order__source AS (
+  SELECT *
+  FROM `duckdata-320210.wide_world_importers.sales__orders`
+)
+
+, fact_sales_order__rename_column AS (
+  SELECT 
+    order_id AS sales_order_id
+    , customer_id
+  FROM fact_sales_order__source
+)
+
+, fact_sales_order__cast_type AS (
+  SELECT 
+    CAST(sales_order_id AS INTEGER) AS sales_order_id
+    , CAST(customer_id AS INTEGER) AS customer_id
+  FROM fact_sales_order__rename_column
+)
+
+
+SELECT 
+  sales_order_id
+  , customer_id
+FROM fact_sales_order__cast_type
