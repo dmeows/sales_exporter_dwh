e064972370b5bff5a3c2dd091582b61546210bd1
Duc Che <ducchetrongminh@gmail.com>
2022-09-13 23:21:48 +0700

    sửa bài #0106b

--- wide_world_importers_dbt/models/analytics/dim_customer.sql
+++ wide_world_importers_dbt/models/analytics/dim_customer.sql
@@ -6,3 +6,20 @@ Yêu cầu #0106b:
 #}
 
 
+WITH dim_customer__source AS (
+  SELECT * 
+  FROM `duckdata-320210.wide_world_importers.sales__customers`
+)
+
+, dim_customer__cast_type AS (
+  SELECT 
+    CAST(customer_id AS INTEGER) AS customer_id
+    , CAST(customer_name AS STRING) AS customer_name
+  FROM dim_customer__source
+)
+
+
+SELECT 
+  customer_id
+  , customer_name 
+FROM dim_customer__cast_type
