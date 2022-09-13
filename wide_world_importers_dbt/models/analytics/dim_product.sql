7d5c0e9fd5cce8b8e5e10499d6660ea5216c7041
Duc Che <ducchetrongminh@gmail.com>
2022-09-14 00:32:18 +0700

    sửa bài 0108a: thêm is chiller stock

--- wide_world_importers_dbt/models/analytics/dim_product.sql
+++ wide_world_importers_dbt/models/analytics/dim_product.sql
@@ -14,6 +14,7 @@ WITH dim_product__source AS (
     stock_item_id AS product_id
     , stock_item_name AS product_name
     , brand AS brand_name
+    , is_chiller_stock AS is_chiller_stock_boolean
     , supplier_id
   FROM dim_product__source
 )
@@ -23,16 +24,29 @@ WITH dim_product__source AS (
     CAST(product_id AS INTEGER) AS product_id
     , CAST(product_name AS STRING) AS product_name
     , CAST(brand_name AS STRING) AS brand_name
+    , CAST(is_chiller_stock_boolean AS BOOLEAN) AS is_chiller_stock_boolean
     , CAST(supplier_id AS INTEGER) AS supplier_id
   FROM dim_product__rename_column
+)
+
+, dim_product__convert_boolean AS (
+  SELECT 
+    *
+    , CASE
+      WHEN is_chiller_stock_boolean IS TRUE THEN 'Chiller Stock'
+      WHEN is_chiller_stock_boolean IS FALSE THEN 'Not Chiller Stock'
+      ELSE 'Undefined' END
+      AS is_chiller_stock
+  FROM dim_product__cast_type
 )
 
 SELECT 
   dim_product.product_id
   , dim_product.product_name
   , dim_product.brand_name
+  , dim_product.is_chiller_stock
   , dim_product.supplier_id
   , dim_supplier.supplier_name
-FROM dim_product__cast_type AS dim_product
+FROM dim_product__convert_boolean AS dim_product
 LEFT JOIN {{ ref('dim_supplier') }}
   ON dim_product.supplier_id = dim_supplier.supplier_id
