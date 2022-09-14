cc2e5c1519197b70f7474935fecfbbd65a2447ca
Duc Che <ducchetrongminh@gmail.com>
2022-09-14 07:04:14 +0700

    sửa bài 0110a: xử lý dim_person

--- wide_world_importers_dbt/models/analytics/dim_person.sql
+++ wide_world_importers_dbt/models/analytics/dim_person.sql
@@ -11,3 +11,27 @@ Yêu cầu #0110a:
 #}
 
 
+WITH dim_person__source AS (
+  SELECT * 
+  FROM `duckdata-320210.wide_world_importers.application__people`
+)
+
+, dim_person__rename_column AS (
+  SELECT 
+    person_id
+    , full_name AS person_full_name
+  FROM dim_person__source
+)
+
+, dim_person__cast_type AS (
+  SELECT 
+    CAST(person_id AS INTEGER) AS person_id
+    , CAST(person_full_name AS STRING) AS person_full_name
+  FROM dim_person__rename_column
+)
+
+
+SELECT 
+  person_id
+  , person_full_name 
+FROM dim_person__cast_type
