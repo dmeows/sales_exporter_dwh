367f0ebf6f52083b14337fea5bf33309b7438f0b
Duc Che <ducchetrongminh@gmail.com>
2022-09-14 07:27:45 +0700

    sửa bài 0110b: thêm undefined record person

--- wide_world_importers_dbt/models/analytics/dim_person.sql
+++ wide_world_importers_dbt/models/analytics/dim_person.sql
@@ -22,8 +22,20 @@ WITH dim_person__source AS (
   FROM dim_person__rename_column
 )
 
+, dim_person__add_undefined_record AS (
+  SELECT 
+    person_id
+    , person_full_name
+  FROM dim_person__cast_type
+  
+  UNION ALL
+  SELECT 
+    0 AS person_id
+    , 'Undefined' AS person_full_name
+)
+
 
 SELECT 
   person_id
   , person_full_name 
-FROM dim_person__cast_type
+FROM dim_person__add_undefined_record
