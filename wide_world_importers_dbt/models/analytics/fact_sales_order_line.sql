81808d56f62b43c0d6298773d7d1fcfa22a48b4b
Duc Che <ducchetrongminh@gmail.com>
2022-09-14 07:54:49 +0700

    bài tập 0111b

--- wide_world_importers_dbt/models/analytics/fact_sales_order_line.sql
+++ wide_world_importers_dbt/models/analytics/fact_sales_order_line.sql
@@ -1,4 +1,9 @@
 {#
+Yêu cầu #0111b: Lấy thêm dữ liệu cho bảng này:
+
+| Tên gốc                  | Tên mới    |
+|--------------------------|------------|
+| sales__orders.order_date | order_date |
 
 #}
 
