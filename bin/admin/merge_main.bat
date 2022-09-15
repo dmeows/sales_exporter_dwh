3c19fd988dbbbd2a9db534d49f8e5dfb1ec7a250
Che Trong Minh Duc <ducchetrongminh@gmail.com>
2022-09-16 00:41:05 +0700

    merge main

---
+++ bin/admin/merge_main.bat
@@ -0,0 +1,15 @@
+git checkout lesson-0101    && git merge main
+git checkout lesson-0102    && git merge main
+git checkout lesson-0103    && git merge main
+git checkout lesson-0104    && git merge main
+git checkout lesson-0105    && git merge main
+git checkout lesson-0106    && git merge main
+git checkout lesson-0107    && git merge main
+git checkout lesson-0108    && git merge main
+git checkout lesson-0109    && git merge main
+git checkout lesson-0110a   && git merge main
+git checkout lesson-0110b   && git merge main
+git checkout lesson-0111    && git merge main
+git checkout lesson-0112    && git merge main
+
+git push --all
