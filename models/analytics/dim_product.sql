48da241290e43b7563b98cdb9e23a610c7e0e620
Che Trong Minh Duc <ducchetrongminh@gmail.com>
2022-09-16 00:18:04 +0700

    yêu cầu 0105

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,15 +1,14 @@
 {#
-Trong thực tế, data source khi được đưa về data lake có thể không phải là data type (loại dữ liệu) đúng như mình nghĩ. Một ví dụ thường gặp đó là một cột dạng số (numeric) khi đưa về data lake bị chuyển thành dạng chữ (text). Do đó, để ĐẢM BẢO đúng data type, chúng ta cần phải chuyển đổi data type, dù hiện tại có thể nó đang đúng. 
+Khi mình nhìn vào model hiện tại, mọi người sẽ thấy nó rối hơn. Các hàm lồng vào nhau giống như trên Excel (ví dụ UPPER(TRIM(CAST(brand AS STRING)))). Một số cột được dùng ở nhiều nơi, khi sửa phải cẩn thận để không bị mất đồng bộ (ví dụ CAST(quantity AS NUMERIC) được dùng ở 2 nơi là quantity và gross_amount, khi sửa phải sửa 2 nơi).
 
-Yêu cầu #0104a:
-- Tìm syntax chuyển đổi data type
-- Tìm đọc các loại data type của BigQuery
-- Chuyển đổi data type của bảng này
+Yêu cầu #0105a:
+- Tìm cách để quản lý tốt hơn, giảm sự rối rắm cho model này
+
 #}
 
 
 SELECT 
-    stock_item_id AS product_id
-    , stock_item_name AS product_name
-    , brand AS brand_name
+  CAST(stock_item_id AS INTEGER) AS product_id
+  , CAST(stock_item_name AS STRING) AS product_name
+  , CAST(brand AS STRING) AS brand_name
 FROM `duckdata-320210.wide_world_importers.warehouse__stock_items`
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,15 +1,15 @@
 {#
-Yêu cầu #0104b:
-- Chuyển đổi data type của bảng này
+Yêu cầu #0105b:
+- Giảm sự rối rắm cho model này
 
 #}
 
 
 
 SELECT 
-  order_line_id AS sales_order_line_id
-  , stock_item_id AS product_id
-  , quantity
-  , unit_price
-  , quantity * unit_price AS gross_amount
+  CAST(order_line_id AS INTEGER) AS sales_order_line_id
+  , CAST(stock_item_id AS INTEGER) AS product_id
+  , CAST(quantity AS NUMERIC) AS quantity 
+  , CAST(unit_price AS NUMERIC) AS unit_price
+  , CAST(quantity AS NUMERIC) * CAST(unit_price AS NUMERIC) AS gross_amount
 FROM `duckdata-320210.wide_world_importers.sales__order_lines`
