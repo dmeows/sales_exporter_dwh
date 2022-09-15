1ad2602b2a3b833dffa77f4ef947b76bb3ee1d81
Che Trong Minh Duc <ducchetrongminh@gmail.com>
2022-09-16 00:20:09 +0700

    sửa yêu cầu 0106b

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,8 +1,12 @@
 {#
 Yêu cầu #0106b:
 - Xem thông tin và dữ liệu của bảng "sales__customers"
-- Viết câu query để lấy 2 cột: "customer_id", "customer_name"
+- Sửa câu query SQL để lấy 2 cột: "customer_id", "customer_name"
 
 #}
 
 
+
+SELECT 
+  *
+FROM `duckdata-320210.wide_world_importers.sales__customers`
