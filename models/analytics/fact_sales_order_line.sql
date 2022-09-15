8bc6ef2ab9d4b561625fc586ff4b0ba57578cb37
Che Trong Minh Duc <ducchetrongminh@gmail.com>
2022-09-16 00:09:09 +0700

    revise yêu cầu

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,7 @@
 {#
 Ở bài trước, mình đã viết query để lấy 3 cột và đặt lại tên. Trong bài này, mình cần phải thêm "derived facts" (số liệu được tính toán).
-Yêu cầu:
-- Viết query để có thể tính gross_amount (doanh thu) trên model
+Yêu cầu #0102:
+- Sửa query để có thể tính gross_amount (doanh thu) trên model
 #}
 
 
