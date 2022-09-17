f1f7a28f2a60e11bf143f64183dff26cc90c4dd4
khanh <habaokhanh239@gmail.com>
2022-09-17 18:02:34 +0700

    add calculate cte

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -9,28 +9,35 @@ Yêu cầu #0105b:
 
 
 WITH fact_sales_order_line__source AS(
-  SELECT *
+  SELECT 
+    *
   FROM `duckdata-320210.wide_world_importers.sales__order_lines`
 ),
 
 fact_sales_order_line__rename AS(
-  SELECT order_line_id AS sales_order_line_id
-  , quantity
-  , unit_price
-  , stock_item_id AS product_id
-  , quantity * unit_price AS gross_amount
+  SELECT 
+    order_line_id AS sales_order_line_id
+    , quantity
+    , unit_price
+    , stock_item_id AS product_id
   FROM fact_sales_order_line__source
 ),
 
 fact_sales_order_line__cast AS(
-  SELECT CAST(sales_order_line_id AS INTEGER) AS sales_order_line_id
-  , CAST(product_id AS INTEGER) AS product_id
-  , CAST(quantity AS NUMERIC) AS quantity 
-  , CAST(unit_price AS NUMERIC) AS unit_price
-  , CAST(gross_amount AS NUMERIC) AS gross_amount
+  SELECT 
+    CAST(sales_order_line_id AS INTEGER) AS sales_order_line_id
+    , CAST(product_id AS INTEGER) AS product_id
+    , CAST(quantity AS NUMERIC) AS quantity 
+    , CAST(unit_price AS NUMERIC) AS unit_price
+  FROM fact_sales_order_line__rename
+)
+
+fact_sales_order_line__calculate AS(
+  SELECT *,
+    quantity*unit_price as gross_rev
   FROM fact_sales_order_line__rename
 )
 
 SELECT *
-FROM fact_sales_order_line__cast
+FROM fact_sales_order_line__calculate
 
