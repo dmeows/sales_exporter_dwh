75f3051503a51544b4f0e01c66011adf69dd20d2
khanh <habaokhanh239@gmail.com>
2022-09-17 16:20:05 +0700

    bai tap 2

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -10,4 +10,5 @@ SELECT
   order_line_id AS sales_order_line_id
   , quantity
   , unit_price
+  , quantity * unit_price AS gross_amount
 FROM `duckdata-320210.wide_world_importers.sales__order_lines`
