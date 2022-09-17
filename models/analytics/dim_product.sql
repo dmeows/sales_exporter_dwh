f8bd00f0bb4b7f23510db48ec9d4eefcc85388e9
khanh <habaokhanh239@gmail.com>
2022-09-17 18:03:57 +0700

    space for dim table

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -11,21 +11,24 @@ Yêu cầu #0105a:
 #}
 
 WITH dim_product__source AS(
-  SELECT * 
+  SELECT 
+    * 
   FROM `duckdata-320210.wide_world_importers.warehouse__stock_items`
 ),
 
 dim_product__rename AS(
-  SELECT stock_item_id AS product_id
-  , stock_item_name AS product_name
-  , brand AS brand_name
+  SELECT 
+    stock_item_id AS product_id
+    , stock_item_name AS product_name
+    , brand AS brand_name
 FROM dim_product__source
 ),
 
 dim_product__cast AS(
-  SELECT CAST(product_id AS INTEGER) AS product_id
-  , CAST(product_name AS STRING) AS product_name
-  , CAST(brand_name AS STRING) AS brand_name
+  SELECT 
+    CAST(product_id AS INTEGER) AS product_id
+    , CAST(product_name AS STRING) AS product_name
+    , CAST(brand_name AS STRING) AS brand_name
   FROM dim_product__rename
 )
 SELECT * 
