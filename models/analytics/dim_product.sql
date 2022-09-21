8d48410829b54348aea7b86c44d3676f0d16fb55
khanh <habaokhanh239@gmail.com>
2022-09-21 20:48:22 +0700

    dim supplier

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -30,19 +30,25 @@ WITH dim_product__source AS (
     stock_item_id AS product_id
     , stock_item_name AS product_name
     , brand AS brand_name
+    , supplier_id
   FROM dim_product__source
 )
 
 , dim_product__cast_type AS (
   SELECT 
     CAST(product_id AS INTEGER) AS product_id
+    , CAST(supplier_id AS INTEGER) AS supplier_id
     , CAST(product_name AS STRING) AS product_name
     , CAST(brand_name AS STRING) AS brand_name
   FROM dim_product__rename_column
 )
 
 SELECT 
-  product_id
-  , product_name
-  , brand_name
-FROM dim_product__cast_type
+  dim_product.product_id
+  , dim_product.product_name
+  , dim_product.brand_name
+  , dim_supplier.supplier_id
+  , dim_supplier.supplier_name
+FROM dim_product__cast_type dim_product
+LEFT JOIN {{ ref('dim_supplier') }}  dim_supplier
+    ON dim_product.supplier_id = dim_supplier.supplier_id
--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -11,3 +11,19 @@ Yêu cầu #0107a:
 #}
 
 
+WITH dim_supplier__source AS (
+  SELECT * 
+  FROM `duckdata-320210.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplier__cast_type AS (
+  SELECT 
+    CAST(supplier_id AS INTEGER) AS supplier_id
+    , CAST(supplier_name AS STRING) AS supplier_name
+  FROM dim_supplier__source
+)
+
+SELECT 
+  supplier_id
+  , supplier_name
+FROM dim_supplier__cast_type
