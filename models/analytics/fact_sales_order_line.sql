a4aab7e851a3cef1b8afb5353c32f916f191474c
ducchetrongminh <ducchetrongminh@gmail.com>
2022-09-24 11:31:45 +0700

    xác định dữ liệu mới bằng is_incremental

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -63,3 +63,7 @@ SELECT
 FROM fact_sales_order_line__calculate_fact AS fact_line
 LEFT JOIN {{ ref('stg_fact_sales_order') }} AS fact_header
   ON fact_line.sales_order_id = fact_header.sales_order_id
+
+{% if is_incremental() %}
+  WHERE fact_line.last_edited_when >= (SELECT MAX(last_edited_when) FROM {{ this }})
+{% endif %}
