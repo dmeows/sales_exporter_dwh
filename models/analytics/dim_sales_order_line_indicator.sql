3c3eb85975533034e882d2418ac6e1fdf3f7b72a
ducchetrongminh <ducchetrongminh@gmail.com>
2022-09-28 12:41:08 +0700

    nâng cao 0204: dùng EXISTS để làm sạch option

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -21,15 +21,36 @@ WITH dim_package_type AS (
     , 'Undefined' AS is_undersupply_backordered
 )
 
-SELECT 
-  FARM_FINGERPRINT(CONCAT(
-    dim_is_undersupply_backordered.is_undersupply_backordered_boolean
+, dim_sales_order_line_indicator__generate AS (
+  SELECT 
+    FARM_FINGERPRINT(CONCAT(
+      dim_is_undersupply_backordered.is_undersupply_backordered_boolean
+      , dim_package_type.package_type_id
+    )) AS sales_order_line_indicator_key
+    
+    , dim_is_undersupply_backordered.is_undersupply_backordered_boolean
+    , dim_is_undersupply_backordered.is_undersupply_backordered
     , dim_package_type.package_type_id
-  )) AS sales_order_line_indicator_key
-  
-  , dim_is_undersupply_backordered.is_undersupply_backordered_boolean
-  , dim_is_undersupply_backordered.is_undersupply_backordered
-  , dim_package_type.package_type_id
-  , dim_package_type.package_type_name
-FROM dim_is_undersupply_backordered
-CROSS JOIN dim_package_type
+    , dim_package_type.package_type_name
+  FROM dim_is_undersupply_backordered
+  CROSS JOIN dim_package_type
+)
+
+, dim_sales_order_line_indicator__cleanse AS (
+  SELECT *
+  FROM dim_sales_order_line_indicator__generate AS dim_sales_order_line_indicator
+  WHERE EXISTS (
+    SELECT 1 FROM {{ ref('fact_sales_order_line') }}
+    WHERE 
+      fact_sales_order_line.sales_order_line_indicator_key = dim_sales_order_line_indicator.sales_order_line_indicator_key
+  )
+)
+
+SELECT 
+  sales_order_line_indicator_key
+    
+  , is_undersupply_backordered_boolean
+  , is_undersupply_backordered
+  , package_type_id
+  , package_type_name
+FROM dim_sales_order_line_indicator__cleanse
