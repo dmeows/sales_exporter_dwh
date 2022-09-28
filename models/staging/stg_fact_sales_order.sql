8ed58cf3900415b269880a7b9bb602ff7ca5eee3
ducchetrongminh <ducchetrongminh@gmail.com>
2022-09-28 11:51:31 +0700

    làm bài 0203a: thêm salesperson person id

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -8,6 +8,7 @@ WITH fact_sales_order__source AS (
     order_id AS sales_order_id
     , customer_id
     , picked_by_person_id
+    , salesperson_person_id
     , order_date
     , last_edited_when AS sales__orders__last_edited_when
   FROM fact_sales_order__source
@@ -18,6 +19,7 @@ WITH fact_sales_order__source AS (
     CAST(sales_order_id AS INTEGER) AS sales_order_id
     , CAST(customer_id AS INTEGER) AS customer_id
     , CAST(picked_by_person_id AS INTEGER) AS picked_by_person_id
+    , CAST(salesperson_person_id AS INTEGER) AS salesperson_person_id
     , CAST(order_date AS DATE) AS order_date
     , CAST(sales__orders__last_edited_when AS TIMESTAMP) AS sales__orders__last_edited_when
   FROM fact_sales_order__rename_column
@@ -28,6 +30,7 @@ SELECT
   sales_order_id
   , customer_id
   , picked_by_person_id
+  , salesperson_person_id
   , order_date
   , sales__orders__last_edited_when
 FROM fact_sales_order__cast_type
