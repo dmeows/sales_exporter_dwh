2a607eead3955e5f6a8d8376ea0d81b28fcbfa9d
Dmeows <habaokhanh239@gmail.com>
2022-10-30 10:57:07 +0700

    year to year target

--- models/analytics/fact_category_target.sql
+++ models/analytics/fact_category_target.sql
@@ -1,15 +1,25 @@
+WITH yearly_revenue AS (
+  SELECT
+    FORMAT_DATE('%Y-%m',order_date) AS month
+    , sum(gross_amount) as actual_revenue
+  FROM {{ ref('fact_sales_order_line')}} fact_sales_order_line
+  GROUP BY 1
+)
 
-SELECT 
-  FORMAT_DATE('%Y', date(fact_sales_order_line.order_date)) as year
-  , dim_product.product_name
-  , SUM(fact_sales_order_line.gross_amount) as gross_amount
-  , SUM(target_revenue.target_revenue) as target_revenue
+, target_rev AS (
+  SELECT  
+    FORMAT_DATE('%Y-%m',year) as month
+    , sum(target_revenue) as target_revenue
+  FROM {{ ref('stg_external__category_target')}} target_rev
+  GROUP BY 1
+)
 
-FROM {{ ref('fact_sales_order_line')}} fact_sales_order_line 
-LEFT JOIN {{ ref('dim_product')}} dim_product 
-  ON fact_sales_order_line.product_id = dim_product.product_id
-LEFT JOIN {{ ref('stg_external__category_target')}} target_revenue
-  ON dim_product.category_id = target_revenue.category_id
-  AND FORMAT_DATE('%Y', date(fact_sales_order_line.order_date)) = FORMAT_DATE('%Y', date(target_revenue.year))
+SELECT  distinct 
+  dim_month_map_bridge.current_month
+  , dim_month_map_bridge.type
+  , yearly_revenue.actual_revenue
+  , target_rev.target_revenue
 
-GROUP BY 1,2
+FROM {{ ref('dim_month_map_bridge') }} dim_month_map_bridge
+LEFT JOIN yearly_revenue ON dim_month_map_bridge.month_original = yearly_revenue.month
+LEFT JOIN target_rev ON dim_month_map_bridge.month_original = target_rev.month
